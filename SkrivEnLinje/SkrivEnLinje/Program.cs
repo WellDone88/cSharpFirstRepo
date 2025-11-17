055f33aa6dac00e519a5826ed4021e0f2258383d
WellDone88 <kenneth.moren@live.no>
2025-11-17 13:21:08 +0100

    Update Program.cs

--- SkrivEnLinje/SkrivEnLinje/Program.cs
+++ SkrivEnLinje/SkrivEnLinje/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hei på deg!");
+﻿namespace SkrivEnLinje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei hva heter du?");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Kenneth {input}");
+            var answer = Console.ReadLine();
+            Console.WriteLine($"Kult navn! {answer}");
+        }
+    }
+}
