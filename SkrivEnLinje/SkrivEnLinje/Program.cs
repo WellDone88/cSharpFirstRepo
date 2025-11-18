ae6adc49870dcab80483330b5e1e3b99a09145cc
WellDone88 <kenneth.moren@live.no>
2025-11-18 12:54:44 +0100

    fdsf

--- SkrivEnLinje/SkrivEnLinje/Program.cs
+++ SkrivEnLinje/SkrivEnLinje/Program.cs
@@ -6,9 +6,13 @@
         {
             Console.WriteLine("Hei hva heter du?");
             var input = Console.ReadLine();
-            Console.WriteLine($"Hei {input}!");
+            Console.WriteLine($"Hygglig å møte deg {input}, hva kan jeg hjelpe deg med i dag?");
             var answer = Console.ReadLine();
-            Console.WriteLine($"Kult navn! {answer}");
+            Console.WriteLine($"Oki!Da må du bruke google og søke etter: {answer}. Er det noe annet jeg kan hjelpe deg med?"!);
+            Console.ReadLine();
+            Console.WriteLine("Dette gidder jeg ikke, ha en fin dag!");
+
+            return;
         }
     }
 }
